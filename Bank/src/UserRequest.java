1eddc48abcb8afb9e9358f73c6348c4ff87bb060
Max <maxooonzzz@gmail.com>
2024-06-08 17:59:47 +0300

    user2

--- Bank/src/UserRequest.java
+++ Bank/src/UserRequest.java
@@ -1,4 +1,34 @@
 
 public class UserRequest {
 	
+	private int loans;
+	private int id;
+	private String message;
+	private int amount;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getLoans() {
+		return loans;
+	}
+	public void setLoans(int loans) {
+		this.loans = loans;
+	}
+	
 }
