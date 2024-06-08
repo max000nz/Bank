bd175f3803e4b36733b1a3c95eb2f3d430b68885
bengo <bengo@GOFMAN-PC>
2024-06-08 17:29:17 +0300

    Admin & Roles Class

---
+++ Bank/src/Admin.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+
+public class Admin extends Roles {
+public LinkedList<User> users=new LinkedList<User>(); 
+//public Stack<Request> bankReq=new Stack<Request>();
+//public Queue<BankLoans> allBankLoans=new Queue<BankLoans>();
+//public Queue<BankDeposite>allBankDeposite=new Queue<BankDeposite>();
+	
+	
+//functions
+	
+private User GetUsers() {
+return this.Users;
+}
+
+
+private BankDeposite GetTransactionHistory() {
+return this.allBankDeposite;	
+}
+
+private void DeleteUser(String id)
+{
+	
+}
+
+
+//private GetAnalytics(){
+//implementation of Binary tree  
+//}
+
+
+private boolean ApproveRequest()
+{
+	
+}
+}
---
+++ Bank/src/Roles.java
@@ -0,0 +1,21 @@
+public class Roles {
+String name="NULL";
+String lastName="NULL";
+String Password="NULL";
+int id=0;
+
+private enum roleType {
+	User, 
+	Admin;
+}
+
+
+
+//functions
+public boolean SearchInfo(int id, String Password)
+{
+	
+	return true;
+}
+
+}
