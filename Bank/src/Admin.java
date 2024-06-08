da02eb34b86a98aa0759ae6523abf0c0d7822911
Max <maxooonzzz@gmail.com>
2024-06-08 19:06:28 +0300

    admin

--- Bank/src/Admin.java
+++ Bank/src/Admin.java
@@ -41,47 +41,4 @@ private boolean ApproveRequest()
 {
 
 }
-}import java.util.*;
-
-public class Admin extends Roles {
-public LinkedList<User> users=new LinkedList<User>(); 
-public Stack<Request> bankReq=new Stack<Request>();
-public Queue<BankLoans> allBankLoans=new LinkedList<BankLoans>();
-public Queue<BankDeposite>allBankDeposite=new Queue<BankDeposite>();
-  
-//gay
-//functions
-
-private User GetUsers() {
-return this.users.getFirst();
-}
-
-
-private BankDeposite GetTransactionHistory() {
-
-}
-
-private void DeleteUser(int id)
-{
-for(int i=0; i<this.users.size(); i++){
-this.users.get(i);
-if(Roles.id==id) this.users.remove(i);
-else
-{
-System.out.println("User Not Found");
-}
-
-}
-}
-
-
-//private GetAnalytics(){
-//implementation of Binary tree  
-//}
-
-
-private boolean ApproveRequest()
-{
-
-}
 }
