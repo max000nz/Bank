54b13d81438c1267bc60567ff4d85ece576cb74f
Max <maxooonzzz@gmail.com>
2024-06-08 19:04:39 +0300

    updater

--- Bank/src/Admin.java
+++ Bank/src/Admin.java
@@ -1,28 +1,34 @@
-
 import java.util.*;
-
 
 public class Admin extends Roles {
 public LinkedList<User> users=new LinkedList<User>(); 
-//public Stack<Request> bankReq=new Stack<Request>();
-//public Queue<BankLoans> allBankLoans=new Queue<BankLoans>();
-//public Queue<BankDeposite>allBankDeposite=new Queue<BankDeposite>();
-	
-	
+public Stack<Request> bankReq=new Stack<Request>();
+public Queue<BankLoans> allBankLoans=new LinkedList<BankLoans>();
+public Queue<BankDeposite>allBankDeposite=new Queue<BankDeposite>();
+  
+
 //functions
-	
+
 private User GetUsers() {
-return this.Users;
+return this.users.getFirst();
 }
 
 
 private BankDeposite GetTransactionHistory() {
-return this.allBankDeposite;	
+
 }
 
-private void DeleteUser(String id)
+private void DeleteUser(int id)
 {
-	
+for(int i=0; i<this.users.size(); i++){
+this.users.get(i);
+if(Roles.id==id) this.users.remove(i);
+else
+{
+System.out.println("User Not Found");
+}
+
+}
 }
 
 
@@ -33,6 +39,49 @@ private void DeleteUser(String id)
 
 private boolean ApproveRequest()
 {
-	
+
+}
+}import java.util.*;
+
+public class Admin extends Roles {
+public LinkedList<User> users=new LinkedList<User>(); 
+public Stack<Request> bankReq=new Stack<Request>();
+public Queue<BankLoans> allBankLoans=new LinkedList<BankLoans>();
+public Queue<BankDeposite>allBankDeposite=new Queue<BankDeposite>();
+  
+//gay
+//functions
+
+private User GetUsers() {
+return this.users.getFirst();
+}
+
+
+private BankDeposite GetTransactionHistory() {
+
+}
+
+private void DeleteUser(int id)
+{
+for(int i=0; i<this.users.size(); i++){
+this.users.get(i);
+if(Roles.id==id) this.users.remove(i);
+else
+{
+System.out.println("User Not Found");
+}
+
+}
+}
+
+
+//private GetAnalytics(){
+//implementation of Binary tree  
+//}
+
+
+private boolean ApproveRequest()
+{
+
 }
 }
