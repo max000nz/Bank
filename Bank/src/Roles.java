801ae96bf40d77ee22f4d419cd1847cd457775ce
bengo <bengo@GOFMAN-PC>
2024-06-08 18:44:49 +0300

    Admin & Roles --Ben

--- Bank/src/Roles.java
+++ Bank/src/Roles.java
@@ -1,21 +1,50 @@
 public class Roles {
-String name="NULL";
-String lastName="NULL";
-String Password="NULL";
-int id=0;
+private String name;
+private String lastName;
+private String Password;
+static int id=0;
 
 private enum roleType {
-	User, 
-	Admin;
+User, 
+Admin;
 }
-
+  
 
 
 //functions
+public Roles()
+{
+id=id+1;
+}
+
 public boolean SearchInfo(int id, String Password)
 {
-	
-	return true;
+
+return true;
+}
+
+public String getName() {
+return name;
+}
+
+public void setName(String name) {
+this.name = name;
+}
+
+public String getLastName() {
+return lastName;
+}
+
+public void setLastName(String lastName) {
+this.lastName = lastName;
+}
+
+public String getPassword() {
+return Password;
+}
+
+public void setPassword(String password) {
+Password = password;
 }
 
 }
