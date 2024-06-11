33fbaf4d9f017a8ce21605576374a62884cc5568
Etgar Avshalomov <EtgarAvshalomov@outlook.com>
2024-06-11 16:15:51 +0300

    validation changes

--- Bank/src/menus/UserMenu.java
+++ Bank/src/menus/UserMenu.java
@@ -5,11 +5,7 @@ import bank_classes.BankRequest;
 import bank_classes.UserRequest;
 import connection.RoleAns;
 import enums.RequestType;
-
-import java.util.Queue;
-import java.util.Scanner;
 import java.util.UUID;
-
 import role_classes.User;
 
 public class UserMenu {
@@ -17,7 +13,7 @@ public class UserMenu {
 
         float changeCash = 0;
         float currCash = 0;
-        String message;
+        String message = "";
         System.out.println("Welcome, " + currUser.getName());
 
         System.out.println("1.Withdrawal");
@@ -26,26 +22,49 @@ public class UserMenu {
         System.out.println("4.Long deposit");
         System.out.println("5.Close loan");
         System.out.println("6.Close long deposit");
-        int choice = RoleAns.intInput("What you want to do?", 1, 4);
+        System.out.println("0.Exit");
+        int choice = -1;
+        try {
+            choice = RoleAns.choiceInput("What do you want to do?", 1, 7);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        
         switch (choice) {
 
             case 1:
                 currCash = currUser.GetCash();
-                changeCash = RoleAns.floatInput("How much money you want to withdrawal?", 1, currCash);
+                try {
+                    changeCash = RoleAns.cashInput("How much money you want to withdrawal?", 1, currCash);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
                 currCash -= changeCash;
                 currUser.SetCash(currCash);
                 break;
 
+
             case 2:
-                changeCash = RoleAns.floatInput("How much money you want to deposit?", 1, 50000);
+            
+                try {
+                    changeCash = RoleAns.cashInput("How much money you want to deposit?", 1, 50000);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
                 currCash = currUser.GetCash();
                 currCash += changeCash;
                 currUser.SetCash(currCash);
                 break;
 
+
             case 3:
-                changeCash = RoleAns.floatInput("How much money you want to loan?", 1, 50000);
-                message = RoleAns.stringInput("Why you want to take a loan?", 1, 100, true);
+                
+                try {
+                    changeCash = RoleAns.cashInput("How much money you want to loan?", 1, 50000);
+                    message = RoleAns.messageInput("Why you want to take a loan?", 1, 100);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
                 UserRequest loan = new UserRequest();
                 loan.setAmount(changeCash);
                 loan.setMessage(message);
@@ -56,8 +75,12 @@ public class UserMenu {
 
             case 4:
                 currCash = currUser.GetCash();
-                changeCash = RoleAns.floatInput("How much money you want to put on long deposit?", 1, currCash);
-                message = RoleAns.stringInput("Why you want to put money on deposit?", 1, 100, true);
+                try {
+                    changeCash = RoleAns.cashInput("How much money you want to put on long deposit?", 1, currCash);
+                    message = RoleAns.messageInput("Why you want to put money on deposit?", 1, 100);
+                } catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
                 UserRequest deposit = new UserRequest();
                 deposit.setAmount(changeCash);
                 deposit.setMessage(message);
@@ -67,13 +90,17 @@ public class UserMenu {
                 break;
             case 5:
                 int num = 1;
-                int loanChoice;
+                int loanChoice = 0;
                 System.out.println("Your current balance is " + currUser.GetCash());
                 for (UUID elem : currUser.getLoans()) {
                     UserRequest curr_request = BankRequest.findRequestById(elem);
                     System.out.printf("%d. %d", num, curr_request.getAmount());
                     num++;
-                    loanChoice = RoleAns.intInput("Do you want to close this loan?\n1.Yes\n2.No\n3.Exit", 1, 3);
+                    try {
+                        loanChoice = RoleAns.choiceInput("Do you want to close this loan?\n1.Yes\n2.No\n3.Exit", 1, 3);
+                    } catch (Exception e) {
+                        System.err.println(e.getMessage());
+                    }
                     switch (loanChoice) {
                         case 1:
                             if (currUser.GetCash() >= curr_request.getAmount()) {
@@ -91,13 +118,17 @@ public class UserMenu {
                 break;
             case 6:
                 num = 1;
-                int depositChoice;
+                int depositChoice = 0;
                 System.out.println("Your current balance is " + currUser.GetCash());
                 for (UUID elem : currUser.getDeposits()) {
                     UserRequest curr_request = BankRequest.findRequestById(elem);
                     System.out.printf("%d. %d", num, curr_request.getAmount());
                     num++;
-                    depositChoice = RoleAns.intInput("Do you want to close this deposit?\n1.Yes\n2.No\n3.Exit", 1, 3);
+                    try {
+                        depositChoice = RoleAns.choiceInput("Do you want to close this deposit?\n1.Yes\n2.No\n3.Exit", 1, 3);
+                    } catch (Exception e) {
+                        System.err.println(e.getMessage());
+                    }
                     switch (depositChoice) {
                         case 1:
                             currUser.WithdrawDepositR(curr_request);
