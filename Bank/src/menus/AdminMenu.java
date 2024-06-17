0f2234a7f79ed848bb04627a74a9d12c8e5bcbe5
bengo <bengo@GOFMAN-PC>
2024-06-17 17:37:57 +0300

    Ben

--- Bank/src/menus/AdminMenu.java
+++ Bank/src/menus/AdminMenu.java
@@ -19,25 +19,26 @@ public abstract  class AdminMenu {
 
     while (working) {
        
-        int userchoice=RoleAns.choiceInput("\n" + "Hello"+ " " + admin.getName()+ " " + admin.getLastName() +"\n1.Show all Users\n2.Show Transaction History\n3.Delete User\n4.Get analytics\n5.Approve Requests\n0.exit\nEnter Choice:", 0, 5, input);
+        int userchoice=RoleAns.choiceInput("\nHello "+admin.getName()+ " " + admin.getLastName() +"\n1.Show all Users\n2.Show Transaction History\n3.Delete User\n4.Get analytics\n5.Approve Requests\n0.exit\nEnter Choice:", 0, 5, input);
         switch (userchoice) {
             case 0:
             return;
             case 1:
-            if(users.size()-1==0) {System.out.println("\n" + "Users list is empty"+ "\n"); break;}
+            if(users.size()-1==0) {System.out.println("\nUsers list is empty\n"); break;}
               for (Roles elem : users) {
                   if(elem.getRole()==enums.RoleType.ADMIN) continue;
                   else{
-                  System.out.println(elem.getId());
-                  System.out.println(elem.getName());
-                  if(elem instanceof User) System.out.println(((User) elem).GetCash());
+                  System.out.println("\n");
+                  System.out.println("User ID: "+elem.getId());
+                  System.out.println("User name: " + elem.getName() + " " + elem.getLastName());
+                  if(elem instanceof User) System.out.println("Current balance: " +((User) elem).GetCash());
                   System.out.println();
                   }
               }
             break;
 
             case 2:
-                int transaction_choice = RoleAns.choiceInput("1.Show Loan History\n2.Show Deposit History", 1, 2, input);
+                int transaction_choice = RoleAns.choiceInput("\n1.Show Loan History\n2.Show Deposit History", 1, 2, input);
                 switch (transaction_choice) {
                   case 1:
                     ThreeBranchTree.printLoanHistory(ThreeBranchTree.getLoanRoot());
@@ -54,12 +55,12 @@ public abstract  class AdminMenu {
             
             case 3:
                 while (true){
-                    int user_id= RoleAns.idInput("\"Please provide an id: ", 0, 999999999, input);
+                    int user_id= RoleAns.idInput("\nPlease provide an id: ", 0, 999999999, input);
                     if(user_id == 0) break; 
                     else if(user_id < 100000000 || user_id > 999999999){
-                        System.out.println("Please enter a 9 digit number");
+                        System.out.println("\nPlease enter a 9 digit number");
                         int choice = -1;
-                        choice = RoleAns.choiceInput("Would you like to continue?\n0.No\n1.Yes", 0, 1, input);
+                        choice = RoleAns.choiceInput("\nWould you like to continue?\n0.No\n1.Yes", 0, 1, input);
                         if(choice == 0) break;
                         continue;
                     }
@@ -80,19 +81,19 @@ public abstract  class AdminMenu {
                 approveLoop = true;
                 while(approveLoop){
                     if(BankRequest.isEmptyRequests() == true){
-                        System.out.println("No more requests, going back to menu");
+                        System.out.println("\nNo more requests, going back to menu");
                         break;
                     }
                     UserRequest curr_req = BankRequest.getFirstRequest();
                     User curr_user = UsersList.findUsertById(curr_req.getUserId());
-                    System.out.println("User Id: " + curr_req.getUserId());
+                    System.out.println("\nUser Id: " + curr_req.getUserId());
                     System.out.println("User Name: " + curr_user.getName());
                     System.out.println("User Last name: " + curr_user.getLastName());
                     System.out.println("Message: " + curr_req.getMessage());
                     System.out.println("Amount: " + curr_req.getAmount());
                     System.out.println("Type: " + curr_req.getType());
                     int choice=-1;
-                    choice = RoleAns.choiceInput("Do you want to approve?\n1.Yes\n2.No\n3.Exit", 1, 3, input);
+                    choice = RoleAns.choiceInput("\nDo you want to approve?\n1.Yes\n2.No\n3.Exit", 1, 3, input);
                     switch(choice){
                         case 1:
                             try {
@@ -103,11 +104,11 @@ public abstract  class AdminMenu {
                                     }
                                     
                                     if(BankRequest.getRequestsSize() == 0){
-                                        System.out.println("No more requests");
+                                        System.out.println("\nNo more requests");
                                         break;
                                     }
                                 }else{
-                                    System.out.println("Cannot approve");
+                                    System.out.println("\nCannot approve");
                                     admin.denyRequest();
                                 }
                             } catch (Exception e) {
